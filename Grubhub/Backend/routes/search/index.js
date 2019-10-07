786dbae386837aa7e4f70e96ccececde8f5e7b4e
prathamr <prathamesh.karve@sjsu.edu>
2019-10-06 20:15:24 -0700

    Added code without connection pooling in commented form

--- Grubhub/Backend/routes/search/index.js
+++ Grubhub/Backend/routes/search/index.js
@@ -9,6 +9,8 @@ router.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
+/* 
+Code used to test without connection pooling commented out
 
 const getConnection = () => {
     return mysql.createConnection({
@@ -17,7 +19,15 @@ const getConnection = () => {
         password: 'cmpe273sjsu',
         database: 'grubhub'
     })
-};
+}; */
+
+const pool = mysql.createPool({
+        connectionLimit: 100,
+        host: 'localhost',
+        user: 'root',
+        password: 'cmpe273sjsu',
+        database: 'grubhub'
+    });
 
 router.get('/', (req, res) => {
     console.log("Get items and their restaurants based on search query ", req.query.search)
@@ -27,12 +37,26 @@ router.get('/', (req, res) => {
     //const queryString = `SELECT INTO Users (first_name, last_name, email, phone, password_hash) VALUES (?, ?, ?, ?, ?)`;
     const queryString = `SELECT * FROM Items WHERE name = ?`;
 
+    pool.getConnection(function(err, conn) {
+        if(err) {
+            res.send("Error occured");
+        } else {
+            conn.query(queryString, [searchQuery], (err, results, fields) => {
+                if(err) {
+                    console.log(`Failed to signup new user ${err}`);
+                    res.sendStatus(500);
+                    return;
+                }
+
+    /*
+    Code used to test without connection pooling commented out
+
     getConnection().query(queryString, [searchQuery], (err, results, fields) => {
         if(err) {
             console.log(`Failed to signup new user ${err}`);
             res.sendStatus(500);
             return;
-        }
+        } */
 
         console.log("Items are", results);
         res.cookie('cookie',"admin",{maxAge: 900000, httpOnly: false, path : '/'});
@@ -41,6 +65,8 @@ router.get('/', (req, res) => {
         });
         res.end("Successful SignUp");
     });
+
+    }});
     
 });
 
