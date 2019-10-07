6bc1bd1e7c3863f26f054937b5a58f4daa270dd4
prathamr <prathamesh.karve@sjsu.edu>
2019-10-06 23:55:17 -0700

    Fixed checkout bug

--- Grubhub/Backend/routes/checkout/index.js
+++ Grubhub/Backend/routes/checkout/index.js
@@ -2,6 +2,28 @@ const router = require('express').Router();
 
 router.post('/', (req, res) => {
     console.log("Create a new order based on items in cart")
-  })
+
+    const items   = req.body.items;
+    const address    = req.body.address;
+    const restID       = req.body.restID;
+    const userID = req.body.UserID; 
+    let queryString = `INSERT INTO Orders (restID, userID, address) VALUES (?, ?, ?)`;
+
+    getConnection().query(queryString, [restID, userID, address], (err, results, fields) => {
+        if(err) {
+            console.log(`Failed to signup new user ${err}`);
+            res.sendStatus(500);
+            return;
+        }
+
+        console.log(`Added new user with id ${results}`);
+        res.cookie('cookie',"admin",{maxAge: 900000, httpOnly: false, path : '/'});
+        res.writeHead(200,{
+            'Content-Type' : 'text/plain'
+        });
+        res.end("Successful SignUp");
+    });
+
+  });
 
 module.exports = router;
---
+++ Grubhub/Frontend/src/components/ShowProfile/Restaurant.js
@@ -0,0 +1,153 @@
+import React, {Component} from 'react';
+import '../../App.css';
+import axios from 'axios';
+import cookie from 'react-cookies';
+import {Redirect} from 'react-router';
+
+let email = localStorage.getItem("email");
+
+class Restaurant extends Component {
+    constructor(){
+        super();
+        this.state = {  
+            user : {}
+        }
+        //Bind the handlers to this class
+        this.firstnameChangeHandler = this.firstnameChangeHandler.bind(this);
+        this.lastnameChangeHandler = this.lastnameChangeHandler.bind(this);
+        this.emailChangeHandler = this.emailChangeHandler.bind(this);
+        this.passwordChangeHandler = this.passwordChangeHandler.bind(this);
+        this.phoneChangeHandler = this.phoneChangeHandler.bind(this);
+        this.submitChange = this.submitChange.bind(this);
+    }
+    //get the books data from backend  
+    componentDidMount(){
+        axios.get(`http://localhost:3003/users/profile?email=${email}`)
+                .then((response) => {
+                //update the state with the response data
+                this.setState({
+                    user : response.data[0]
+                });
+            });
+    }
+
+    //firstname change handler to update state variable with the text entered by the user
+    firstnameChangeHandler = (e) => {
+        this.setState({
+            firstname : e.target.value
+        })
+    }
+
+    //lastname change handler to update state variable with the text entered by the user
+    lastnameChangeHandler = (e) => {
+        this.setState({
+            lastname : e.target.value
+        })
+    }
+
+    //email change handler to update state variable with the text entered by the user
+    emailChangeHandler = (e) => {
+        this.setState({
+            email : e.target.value
+        })
+    }
+    //password change handler to update state variable with the text entered by the user
+    passwordChangeHandler = (e) => {
+        this.setState({
+            password : e.target.value
+        })
+    }
+    //phone change handler to update state variable with the text entered by the user
+    phoneChangeHandler = (e) => {
+        this.setState({
+            phone : e.target.value
+        })
+    }
+    self = this;
+    //submit Change handler to send a request to the node backend
+    submitChange = (e) => {
+        var headers = new Headers();
+        //prevent page from refresh
+        e.preventDefault();
+        const data = {
+            firstname : this.state.firstname,
+            lastname : this.state.lastname,
+            email : this.state.email,
+            password : this.state.password,
+            phone : this.state.phone,
+            image : this.state.image
+        };
+        axios.defaults.withCredentials = true;
+        //make a post request with the user data
+        axios.post('http://localhost:3003/users/profile', data)
+            .then(response => {
+                console.log("Status Code : ",response.status);
+                if(response.status === 200){
+                    this.setState({
+                        authFlag : true
+                    })
+                } else {
+                    console.log("Status Code : ",response.status);
+                    this.setState({
+                        authFlag : false,
+                        loginErr : true
+                    })
+                }
+            }).catch(function (error) {
+                console.log(error);
+                console.log("Logging error");
+           });
+
+           this.setState({
+            authFlag : false,
+            loginErr : true
+        })
+    }
+
+    render(){
+        console.log(email);
+        console.log(this.state.user);
+        //iterate over books to create a table row
+        console.log("Redirection")
+        //if not logged in go to login page
+        let redirectVar = null;
+        if(!cookie.load('cookie')){
+            redirectVar = <Redirect to= "/login"/>
+        }
+        return(
+            <div>
+                {redirectVar}
+            <div class="container">
+                
+                <div class="login-form">
+                    <div class="main-div">
+                        <div class="panel">
+                        {this.state.user.first_name}'s Profile
+                        </div>
+
+                        <img src={this.state.image} height="200" width="200"/>
+
+                        <div class="form-group"> First Name
+                                <input onChange = {this.firstnameChangeHandler} type="text" class="form-control" name="firstname" placeholder={this.state.user.first_name}/>
+                            </div>
+
+                            <div class="form-group"> Last Name 
+                                <input onChange = {this.lastnameChangeHandler} type="text" class="form-control" name="lastname" placeholder={this.state.user.last_name}/>
+                            </div>
+
+                            <div class="form-group"> Email 
+                                <input onChange = {this.emailChangeHandler} type="text" class="form-control" name="email" placeholder={this.state.user.email}/>
+                            </div>
+                            <div class="form-group"> Phone Number
+                                <input onChange = {this.passwordChangeHandler} type="password" class="form-control" name="password" placeholder={this.state.user.phone}/>
+                            </div>
+                            <button onClick = {this.submitChange} class="btn btn-primary">Update</button>                 
+                    </div>
+                </div>
+            </div>
+            </div>  
+        )
+    }
+}
+//export Home Component
+export default Restaurant;
